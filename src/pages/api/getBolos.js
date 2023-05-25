4a8b8bd3f3da104b7d774cb269feeb2aefafc4de
Gonçalo Nogueira <gn16102000@gmail.com>
2023-05-25 17:27:53 +0100

    gonçalo2

--- src/pages/api/getBolos.js
+++ src/pages/api/getBolos.js
@@ -6,7 +6,15 @@ connect()
 export default async function handler(req, res) {
     try {
         const bolos = await Bolo.find({});
-        res.send({ status: 'ok', data: bolos });
+        res.send({
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            },
+            status: 'ok',
+            data: bolos,
+        });
     }
     catch (error) {
         res.status(400).json({ status: 'Not able to get.' })
