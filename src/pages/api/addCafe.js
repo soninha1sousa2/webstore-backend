fbd01ed296dc6c4a4258d6e93a026608e4897f8f
sonia <fc56898@alunos.fc.ul.pt>
2023-06-11 19:34:41 +0100

    cors activated for every enpoint

--- src/pages/api/addCafe.js
+++ src/pages/api/addCafe.js
@@ -7,6 +7,15 @@ export default async function handler(req, res) {
     try {
         console.log(req.body)
         const cafe = await Cafe.create(req.body);
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
         if (!cafe) {
             return res.json({ "code": 'User not created' })
         }
