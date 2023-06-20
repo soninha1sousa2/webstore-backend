309e4a069c0a827de9495c8e3762f4dcefd2d628
BiaPatatas <82343869+BiaPatatas@users.noreply.github.com>
2023-06-20 19:27:13 +0100

    Update deleteBolo.js

--- src/pages/api/deleteBolo.js
+++ src/pages/api/deleteBolo.js
@@ -8,6 +8,10 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
     if (req.method === "DELETE") {
       const { id } = req.query;
       const deletedBolo = await Bolo.findByIdAndDelete(id);
