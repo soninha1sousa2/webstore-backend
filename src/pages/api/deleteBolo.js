02f41b0be6423a9ba5d05e05ecdf44c98df67771
BiaPatatas <82343869+BiaPatatas@users.noreply.github.com>
2023-06-20 19:03:11 +0100

    Update deleteBolo.js

--- src/pages/api/deleteBolo.js
+++ src/pages/api/deleteBolo.js
@@ -1,35 +1,25 @@
 import connect from "../../db/database";
 import Bolo from "../../models/boloSchema";
 
-connect();
+connect()
 
 export default async function handler(req, res) {
-  try {
-    const { id } = req.query;
-
-    const deletedBolo = await Bolo.findByIdAndDelete(id);
-
-    if (!deletedBolo) {
-      return res.status(404).json({ code: "Bolo not found" });
+    try {
+        const bolo = await Bolo.create(req.body);
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (!bolo) {
+            return res.json({ "code": 'User not created' })
+        }
+        return res.json({ "code": "Success!" })
+    } catch (error) {
+        res.status(400).json({ status: 'Not able to create.' })
     }
-
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-
-    return res.status(200).json({ code: "Bolo deleted successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ status: "Not able to delete." });
-  }
 }
-
