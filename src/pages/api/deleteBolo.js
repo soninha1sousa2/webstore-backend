acac28d3a32f9c4596ce177709bfa41a102834ae
BiaPatatas <82343869+BiaPatatas@users.noreply.github.com>
2023-06-20 19:12:07 +0100

    Update deleteBolo.js

--- src/pages/api/deleteBolo.js
+++ src/pages/api/deleteBolo.js
@@ -1,25 +1,22 @@
 import connect from "../../db/database";
 import Bolo from "../../models/boloSchema";
 
-connect()
+connect();
 
 export default async function handler(req, res) {
-    try {
-        const bolo = await Bolo.create(req.body);
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        // another common pattern
-        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        if (!bolo) {
-            return res.json({ "code": 'User not created' })
-        }
-        return res.json({ "code": "Success!" })
-    } catch (error) {
-        res.status(400).json({ status: 'Not able to create.' })
+  try {
+    if (req.method === "DELETE") {
+      const { id } = req.query;
+      const deletedBolo = await Bolo.findByIdAndDelete(id);
+      if (!deletedBolo) {
+        return res.json({ code: "Bolo not found." });
+      }
+      return res.json({ code: "Bolo deleted successfully." });
+    } else {
+      return res.status(405).json({ code: "Method not allowed." });
     }
+  } catch (error) {
+    res.status(400).json({ status: "Not able to delete." });
+  }
 }
+
